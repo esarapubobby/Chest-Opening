19ab11f8921f517b5431f4b3b8ad6de1671bb7b2
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-06-16 15:12:09 +0530

    fixed Obstacle Spawner

--- Assets/Scripts/Environment/ObstacleSpawner.cs
+++ Assets/Scripts/Environment/ObstacleSpawner.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
     [Header("Prefabs")]
     public GameObject[] obstaclePrefabs;
+    private List<GameObject> Obstaclepool=new List<GameObject>();
+    public int poolSize=10;
 
     [Header("Spawn Settings")]
     public Transform player;
@@ -23,7 +27,30 @@ public class ObstacleSpawner : MonoBehaviour
     private void Start()
     {
         currentInterval = initialInterval;
+        for(int i = 0; i < poolSize; i++)
+        {
+            GameObject obstacle=Instantiate(obstaclePrefabs[Random.Range(0,obstaclePrefabs.Length)]);
+            obstacle.SetActive(false);
+            Obstaclepool.Add(obstacle);
+        }
     }
+   GameObject GetObstacle()
+    {
+        List<GameObject> available = new List<GameObject>();
+        foreach (GameObject obstacle in Obstaclepool)
+        {
+            if (!obstacle.activeInHierarchy)
+            {
+                available.Add(obstacle);
+            }
+        }
+        if (available.Count == 0)
+        {
+            return null;
+        }
+        return available[Random.Range(0, available.Count)];
+    }
+
 
     private void Update()
     {
@@ -40,6 +67,16 @@ public class ObstacleSpawner : MonoBehaviour
             SpawnObstacle();
             timer = 0f;
         }
+        foreach (GameObject obstacle in Obstaclepool)
+        {
+            if (obstacle.activeInHierarchy)
+            {
+                if (player.position.z > obstacle.transform.position.z + 10f)
+                {
+                    obstacle.SetActive(false);
+                }
+            }
+        }
     }
 
     private void SpawnObstacle()
@@ -47,15 +84,19 @@ public class ObstacleSpawner : MonoBehaviour
         if (obstaclePrefabs == null || obstaclePrefabs.Length == 0)
             return;
 
-        GameObject prefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
-        if (prefab == null)
-            return;
 
         float lateralOffset = Random.Range(minLateralOffset, maxLateralOffset);
 
         Vector3 spawnPos =
             player.position + player.forward * spawnDistanceAhead + player.right * lateralOffset;
 
-        Instantiate(prefab, spawnPos, Quaternion.identity);
+        GameObject Obstacle = GetObstacle();
+        if (Obstacle == null)
+        {
+            return;
+        }
+        Obstacle.transform.position=spawnPos;
+        Obstacle.transform.rotation= Quaternion.identity;
+        Obstacle.SetActive(true);
     }
 }
